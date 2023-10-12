9a4de3da5667fa0e5dea0beca22c67136e852b1f
catalin-lazarescu <138431234+catalin-lazarescu@users.noreply.github.com>
2023-10-12 15:29:35 +0300

    Add files via upload

--- test.js
+++ test.js
@@ -1,28 +1,35 @@
 
-function requestDeviceOrientation () {
-    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-    DeviceOrientationEvent.requestPermission()
-    .then(permissionState => {
-    if (permissionState === 'granted') {
-    window.addEventListener('deviceorientation', () => {
-        var alpha = evt.alpha; 
-        var beta = evt.beta; 
-        var gamma = evt.gamma; 
-        document.getElementById("a").innerHTML = "alpha = "+alpha; document.getElementById("b").innerHTML = "beta = "+beta; document.getElementById("c").innerHTML = "gamma = "+gamma; 
-        var canvas = document.getElementById("canvas"); 
-        var context = canvas.getContext("2d"); 
-        var centru = {x:canvas.width/2, y:canvas.height/2} 
-        var latura_patrat = 50;
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.fillStyle = `255, ${evt.gamma+100}, ${evt.gamma+100})`;
-        context.beginPath(); 
-        context.fillRect(180, 150, evt.gamma, 50);
-    });
+
+  if ( location.protocol != "https:" ) {
+    location.href = "https:" + window.location.href.substring( window.location.protocol.length );
     }
-    })
-    .catch(console.error);
-    } else {
-    // handle regular non iOS 13+ devices
-    console.log ("not iOS");
+    function permission () {
+        if ( typeof( DeviceMotionEvent ) !== "undefined" && typeof( DeviceMotionEvent.requestPermission ) === "function" ) {
+            // (optional) Do something before API request prompt.
+            DeviceMotionEvent.requestPermission()
+                .then( response => {
+                // (optional) Do something after API prompt dismissed.
+                if ( response == "granted" ) {
+                    window.addEventListener( "devicemotion", (e) => {
+                        var alpha = evt.alpha; 
+                        var beta = evt.beta; 
+                        var gamma = evt.gamma; 
+                        document.getElementById("a").innerHTML = "alpha = "+alpha; document.getElementById("b").innerHTML = "beta = "+beta; document.getElementById("c").innerHTML = "gamma = "+gamma; 
+                        var canvas = document.getElementById("canvas"); 
+                        var context = canvas.getContext("2d"); 
+                        var centru = {x:canvas.width/2, y:canvas.height/2} 
+                        var latura_patrat = 50;
+                        context.clearRect(0, 0, canvas.width, canvas.height);
+                        context.fillStyle = `255, ${evt.gamma+100}, ${evt.gamma+100})`;
+                        context.beginPath(); 
+                        context.fillRect(180, 150, evt.gamma, 50);
+                    })
+                }
+            })
+                .catch( console.error )
+        } else {
+            alert( "DeviceMotionEvent is not defined" );
+        }
     }
-  }
+    const btn = document.getElementById( "request" );
+    btn.addEventListener( "click", permission );
