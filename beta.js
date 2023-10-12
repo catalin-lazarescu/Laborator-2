9bc9566e309157707b58d1eca7258bd32d4f4fa0
catalin-lazarescu <138431234+catalin-lazarescu@users.noreply.github.com>
2023-10-12 15:00:21 +0300

    Add files via upload

--- beta.js
+++ beta.js
@@ -4,7 +4,7 @@ function on_device_orientation(evt)
     var alpha = evt.alpha; 
     var beta = evt.beta; 
     var gamma = evt.gamma; 
-    document.getElementById("a").innerHTML = "alpha = "+alpha; document.getElementById("b").innerHTML = "beta = "+beta; document.getElementById("c").innerHTML = "gamma = "+gamma; 
+    document.getElementById("a").innerHTML = "1alpha = "+alpha; document.getElementById("b").innerHTML = "beta = "+beta; document.getElementById("c").innerHTML = "gamma = "+gamma; 
     var canvas = document.getElementById("canvas"); 
     var context = canvas.getContext("2d"); 
     var centru = {x:canvas.width/2, y:canvas.height/2} 
