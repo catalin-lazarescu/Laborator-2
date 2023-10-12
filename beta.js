64a0245dceb72911a98f925d62b1494550f65354
catalin-lazarescu <138431234+catalin-lazarescu@users.noreply.github.com>
2023-10-12 14:58:09 +0300

    Add files via upload

--- beta.js
+++ beta.js
@@ -10,7 +10,7 @@ function on_device_orientation(evt)
     var centru = {x:canvas.width/2, y:canvas.height/2} 
     var latura_patrat = 50;
     context.clearRect(0, 0, canvas.width, canvas.height);
-    context.fillStyle = "rgb(255, ${evt.gamma+100}, 0)";
+    context.fillStyle = `rgb(255, ${evt.gamma+100}, 0)`;
     context.beginPath(); 
     context.fillRect(180, 150, evt.gamma, 50);
 }
