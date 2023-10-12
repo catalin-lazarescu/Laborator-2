25755946b833bfda85cdeaf090a43aa187a014ff
catalin-lazarescu <138431234+catalin-lazarescu@users.noreply.github.com>
2023-10-12 16:02:13 +0300

    Add files via upload

--- test.js
+++ test.js
@@ -10,9 +10,7 @@
                 .then( response => {
                 // (optional) Do something after API prompt dismissed.
                 if ( response == "granted" ) {
-                    window.addEventListener( "deviceorientation", (e) => {
-						
-                    })
+                    window.addEventListener( "deviceorientation", on_device_orientation)
                 }
             })
                 .catch( console.error )
@@ -20,18 +18,19 @@
             alert( "DeviceOrientationEvent is not defined" );
         }
     }
-    const btn = document.getElementById( "request" );
-    btn.addEventListener( "click", permission );
-
-	var alpha = evt.alpha; 
-	var beta = evt.beta; 
-	var gamma = evt.gamma; 
-	document.getElementById("a").innerHTML = "alpha = "+alpha; document.getElementById("b").innerHTML = "beta = "+beta; document.getElementById("c").innerHTML = "gamma = "+gamma; 
-	var canvas = document.getElementById("canvas"); 
-	var context = canvas.getContext("2d"); 
-	var centru = {x:canvas.width/2, y:canvas.height/2} 
-	var latura_patrat = 50;
-	context.clearRect(0, 0, canvas.width, canvas.height);
-	context.fillStyle = `255, ${evt.gamma+100}, ${evt.gamma+100})`;
-	context.beginPath(); 
-	context.fillRect(180, 150, evt.gamma, 50);
+window.addEventListener("deviceorientation", on_device_orientation); 
+function on_device_orientation(evt) 
+{
+    var alpha = evt.alpha; 
+    var beta = evt.beta; 
+    var gamma = evt.gamma; 
+    document.getElementById("a").innerHTML = "alpha = "+alpha; document.getElementById("b").innerHTML = "beta = "+beta; document.getElementById("c").innerHTML = "gamma = "+gamma; 
+    var canvas = document.getElementById("canvas"); 
+    var context = canvas.getContext("2d"); 
+    var centru = {x:canvas.width/2, y:canvas.height/2} 
+    var latura_patrat = 50;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = `100, ${evt.gamma+100}, 100)`;
+    context.beginPath(); 
+    context.fillRect(180, 150, evt.gamma, 50);
+}
